0aed431591bbedc29c2d204cdfb73e1737a7d553
BalconyJH <balconyjh@gmail.com>
2023-03-22 11:06:09 +0800

    :construction: Adjust the project structure
    Add adapter selection function, change drawing part structure,  add adapter optional configuration items

--- nonebot_plugin_r6s/config.py
+++ nonebot_plugin_r6s/config.py
@@ -1,6 +1,8 @@
 from typing import Optional
 
 from pydantic import BaseSettings
+from nonebot import get_driver
+from .net import get_data_from_r6racker
 
 
 class Config(BaseSettings):
@@ -8,3 +10,8 @@ class Config(BaseSettings):
     r6s_proxy: Optional[str] = None
     r6s_db_username: Optional[str] = None
     r6s_db_password: Optional[str] = None
+    r6s_adapters: Optional[str] = "r6tracker"
+
+
+global_config = get_driver().config
+plugin_config = Config.parse_obj(global_config)
