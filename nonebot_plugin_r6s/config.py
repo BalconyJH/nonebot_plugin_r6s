82ddbe1287b14a7fdae16421c6fbefeb391e237f
BalconyJH <balconyjh@gmail.com>
2024-08-19 21:34:01 +0800

    :wrench: Update configuration model

--- nonebot_plugin_r6s/config.py
+++ nonebot_plugin_r6s/config.py
@@ -1,33 +1,54 @@
 from pathlib import Path
 from typing import Optional
+from urllib.parse import urlparse
 
-from nonebot import get_driver
-from pydantic import BaseSettings, Extra, root_validator
+import httpx
+import nonebot_plugin_localstore as store
+from loguru import logger
+from nonebot import get_driver, get_plugin_config
+from pydantic import BaseModel, model_validator, field_validator
 
 
-class Config(BaseSettings, extra=Extra.ignore):
-    r6s_default_name: str = "MacieJay"
-    r6s_max_retry: int = 3
-    r6s_proxy: Optional[str]
-    r6s_db_username: Optional[str]
-    r6s_db_password: Optional[str]
-    r6s_adapters: Optional[str]
-    r6s_font: Optional[str] = None
-    r6s_cache_dir: Optional[str] = None
+class Config(BaseModel):
+    max_retry: int = 3
+    font_name: str = "JetBrains Mono"
+    proxy: Optional[str] = None
+    resouce_dir: Path = store.get_plugin_data_dir()
+    cache_dir: Path = store.get_plugin_cache_dir()
+    config_dir: Path = store.get_plugin_config_dir()
+    font_path: Path = resouce_dir / "font"
+    image_dir: Path = resouce_dir / "image"
+    database_dir: Path = resouce_dir / "database"
 
-    @root_validator("r6s_font")
-    def set_default_values(cls, values):
-        if not values.get("r6s_font"):
-            values["r6s_font"] = str(
-                Path(__file__).parent / "fonts"
+    @model_validator(mode="after")
+    def ensure_directories_exist(self):
+        directories = [self.font_path, self.image_dir, self.database_dir]
+        for directory in directories:
+            logger.debug(f"Ensuring directory: {directory}")
+            directory.mkdir(parents=True, exist_ok=True)
+        return self
+
+    @field_validator("proxy")
+    def validate_proxy(cls, v: str) -> str:
+        parsed_url = urlparse(v)
+        if parsed_url.scheme not in {"http", "https"}:
+            raise ValueError("Proxy must start with 'http://' or 'https://'.")
+
+        if not parsed_url.hostname or not parsed_url.port:
+            raise ValueError(
+                "Proxy must include both a valid hostname/IP and a port number."
             )
-        if not values.get("r6s_cache_dir"):
-            values["r6s_cache_dir"] = str(
-                Path(__file__).parent / "cache"
-            )
-        if not values.get("r6s_adapters"):
-            values["r6s_adapters"] = "r6tracker"
+
+        logger.info(f"Testing proxy: {v}")
+        try:
+            httpx.get(
+                "http://www.google.com", proxies={"http://": v, "https://": v}
+            ).raise_for_status()
+        except Exception as e:
+            logger.warning(f"Proxy test failed: {e}")
+
+        return v
 
 
 global_config = get_driver().config
-plugin_config = Config.parse_obj(global_config)
+plugin_config = get_plugin_config(Config)
