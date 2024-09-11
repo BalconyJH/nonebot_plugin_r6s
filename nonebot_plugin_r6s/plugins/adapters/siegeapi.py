30761c614852dacc0f46fb337d5403e267482452
BalconyJH <balconyjh@gmail.com>
2024-09-11 21:32:22 +0800

    :recycle: Refactored the SiegeAPI factory function to add the generation method of the relevant data class

--- nonebot_plugin_r6s/plugins/adapters/siegeapi.py
+++ nonebot_plugin_r6s/plugins/adapters/siegeapi.py
@@ -5,7 +5,7 @@ from aiohttp import ClientSession
 from nonebot import logger
 from siegeapi import Auth, Player
 
-from nonebot_plugin_r6s.utils.model import PlayerInfo, LinkAccount
+from nonebot_plugin_r6s.utils.model import PlayerInfo, LinkAccount, UserPreferences
 
 T = TypeVar("T", bound="SiegeAPI")
 
@@ -17,45 +17,59 @@ class SiegeAPI(Auth):
         self.player_name = None
         self.linked_accounts = None
 
-    @overload
     @classmethod
-    async def with_player(
+    async def with_player_by_name(
         cls: type[T],
         email: str,
         password: str,
         session: ClientSession,
         name: str,
-        uid: None = None,
-        platform: Literal["uplay", "xbl", "psn"] = "uplay",
-    ) -> T: ...
-
-    @overload
-    @classmethod
-    async def with_player(
-        cls: type[T],
-        email: str,
-        password: str,
-        session: ClientSession,
-        name: None,
-        uid: str,
-        platform: Literal["uplay", "xbl", "psn"] = "uplay",
-    ) -> T: ...
-
-    @classmethod
-    async def with_player(
-        cls: type[T],
-        email: str,
-        password: str,
-        session: ClientSession,
-        name: Optional[str] = None,
-        uid: Optional[str] = None,
         platform: Literal["uplay", "xbl", "psn"] = "uplay",
     ) -> T:
+        """
+        Create a SiegeAPI instance with a player by name.
+
+        Args:
+            email (str): Ubisoft account email.
+            password (str): Ubisoft account password.
+            session (ClientSession): aiohttp client session.
+            name (str): Player name.
+            platform (Literal["uplay", "xbl", "psn"], optional): Platform. Defaults
+            to "uplay".
+
+        Returns:
+            SiegeAPI: SiegeAPI instance.
+        """
         instance = cls(email, password, session)
         cls.player_name = name
-        instance.player = await instance.get_player(
-            name=name, uid=uid, platform=platform
-        )
+        instance.player = await instance.get_player(name=name, platform=platform)
+        return instance
+
+    @classmethod
+    async def with_player_by_uid(
+        cls: type[T],
+        email: str,
+        password: str,
+        session: ClientSession,
+        uid: str,
+        platform: Literal["uplay", "xbl", "psn"] = "uplay",
+    ) -> T:
+        """
+        Create a SiegeAPI instance with a player by UID.
+
+        Args:
+            email (str): Ubisoft account email.
+            password (str): Ubisoft account password.
+            session (ClientSession): aiohttp client session.
+            uid (str): Player UID.
+            platform (Literal["uplay", "xbl", "psn"], optional): Platform. Defaults
+            to "uplay".
+
+        Returns:
+            SiegeAPI: SiegeAPI instance.
+        """
+        instance = cls(email, password, session)
+        instance.player = await instance.get_player(uid=uid, platform=platform)
         return instance
 
     async def load_data(self):
@@ -99,6 +113,30 @@ class SiegeAPI(Auth):
         self.player = await super().get_player(name=name, uid=uid, platform=platform)
         return self.player
 
+    async def load_linked_accounts(self):
+        if not self.player:
+            raise ValueError(
+                "Player is not set. Please use get_player() to fetch a player first."
+            )
+
+        return [LinkAccount(**vars(account)) for account in self.player.linked_accounts]
+
+    async def load_persona(self) -> Optional[UserPreferences]:
+        if not self.player:
+            raise ValueError(
+                "Player is not set. Please use get_player() to fetch a player first."
+            )
+
+        return (
+            UserPreferences(
+                tag=self.player.persona.tag,
+                enabled=self.player.persona.enabled,
+                nickname=self.player.persona.nickname,
+            )
+            if self.player.persona
+            else None
+        )
+
     async def load_player_info(self) -> PlayerInfo:
         if not self.player:
             raise ValueError(
@@ -106,17 +144,16 @@ class SiegeAPI(Auth):
             )
 
         return PlayerInfo(
-            avatar_url=self.player.profile_pic_url_500,
-            player_name=self.player.name,
-            player_level=self.player.level,
-            player_mmr=getattr(self.player.ranked_profile, "rank_points", 0),
-            aliases=[""],  # TODO: Add aliases (upstream unsupported)
-            linked_accounts=[
-                # LinkAccount(
-                #     platform_type=account.platform_type,
-                #     name_on_platform=account.name_on_platform,
-                # )
-                LinkAccount(**vars(account))
-                for account in self.player.linked_accounts
-            ],
+            avatar_url=self.player.profile_pic_url,
+            name=self.player.name,
+            level=self.player.level,
+            mmr=(
+                self.player.ranked_profile.rank_points
+                if self.player.ranked_profile
+                else 0
+            ),
+            linked_accounts=await self.load_linked_accounts(),
+            total_time_played=self.player.total_time_played_hours,
+            aliases=[""],  # TODO: Add aliases(Upstream unsupported)
+            persona=await self.load_persona(),
         )
